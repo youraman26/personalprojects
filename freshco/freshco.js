e42b6f808e58962db3ff8cda95fbcce7c90fc604
youraman26 <98608618+youraman26@users.noreply.github.com>
2022-07-06 21:50:06 +0530

    All projects Uploaded On git hub

---
+++ freshco/freshco.js
@@ -0,0 +1,52 @@
+gsap.from("#four",{
+    opacity:0,
+    y:200,
+    height: 0,
+    duration:2,
+})
+gsap.from("#line3",{
+    width: "115%",
+    delay: 2,
+    duration: 2,
+})
+gsap.from("#hcol",{
+    opacity:0,
+    y: 50,
+    // height: 0,
+    duration:2,
+})
+gsap.from("#hlux",{
+    opacity:0,
+    y: -50,
+    // height: 0,
+    duration:2,
+})
+
+gsap.from("#heads p",{
+    opacity:0,
+    y: -50,
+    // height: 0,
+    duration:2,
+})
+gsap.from("#n2 a",{
+    opacity:0,
+    y: -50,
+    stagger: .2,
+    // height: 0,
+    duration:2,
+})
+gsap.from("#button #b1 ,#b2",{
+    width:0,
+    height:0,
+    opacity:0,
+    duration:2,
+})
+gsap.from("#button i",{
+    width:0,
+    height:0,
+    opacity:0,
+    delay:1,
+    duration:2,
+})
+
+
